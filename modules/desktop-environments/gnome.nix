405c151881081d8370262d6f0f1057b47dc1fcb5
armorynode <22787155+ArmoryNode@users.noreply.github.com>
2024-08-27 00:05:08 -0700

    Added more changes to `gnome.nix`

--- modules/desktop-environments/gnome.nix
+++ modules/desktop-environments/gnome.nix
@@ -23,6 +23,7 @@
     dash-to-dock
     appindicator
     wireguard-vpn-extension
+    clipboard-history
   ]);
 
   # Install gnome-specific flatpaks
@@ -49,8 +50,8 @@
     enable = true;
     profiles.user.databases = [
       {
-        lockAll = true;
         settings = {
+          # Enable user extensions
           "org/gnome/shell" = {
             disable-user-extensions = false;
             enabled-extensions = with pkgs.gnomeExtensions; [
@@ -60,7 +61,40 @@
               reboottouefi.extensionUuid
               just-perfection.extensionUuid
               wireguard-vpn-extension.extensionUuid
+              clipboard-history.extensionUuid
             ];
+          };
+
+          # Center new windows
+          "org/gnome/mutter" = {
+            center-new-windows=true;
+          };
+
+          # Set favorite apps
+          "org/gnome/shell" = {
+            favorite-apps = [
+              "org.gome.Nautilus.desktop"
+              "firefox.desktop"
+              "com.raggesilver.BlackBox.desktop"
+            ];
+          };
+
+          # Dash to Dock settings
+          "org/gnome/extensions/dash-to-dock" = {
+            apply-custom-theme = true;
+            show-mounts = false;
+            show-show-apps-button = false;
+            show-trash = false;
+          };
+
+          # Rebind notification tray to `Shift+Super+v`
+          "org/gnome/shell/keybindings" = {
+            toggle-message-tray = [ "<Shift><Super>v" ];
+          };
+
+          # Bind clipboard history to `Super+v` (similar to Windows clipboard)
+          "org/gnome/shell/extensions/clipboard-history" = {
+            toggle-menu=[ "<Super>v" ];
           };
         };
       }
